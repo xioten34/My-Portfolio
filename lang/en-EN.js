5f31cbe637c9a4394e1936149831f56c13472836
Damien <damien.jaillot@gmail.com>
2019-12-27 13:50:26 +0700

    lang update

--- lang/en-EN.js
+++ lang/en-EN.js
@@ -24,18 +24,42 @@ export default {
     'main_title': {
       'about_1': `I love webdesign`,
       'about_2': `also front-end design`,
-      'portfolio': `Some of my work`,
+      'portfolio_detail': `One of my projects`,
+      'portfolio_detail_sub_1': `Présentation`,
+      'portfolio_detail_sub_2': `Projet & besoin`,
+      'portfolio_detail_sub_3': `Solutions`,
+      'portfolio': `Some of my works`,
       'travels': `I take my inspiration from travels`
     },
     'text': {
       'about_1': `I love everything related to the web espacially web design. 
                   Front-end developement is also important for me, to be able to realize what i designed. 
                   So I have skills in web design but also in development front, seo etc. I am also interested in video, photo and illustration.`,
-      'about_2': `Currently looking for a job in Quebec.`,
+      'about_2': `Currently looking for a job in France, Quebec or Japan.`,
       'about_3': `Indeed, currently freelance since my last position in Sweden, I am looking for a web designer position in Quebec if possible or in Asia such as Japan.`,
       'travels': `Traveling is one of my passions even though I have not visited many countries. I lived in Japan for 1 year.
                   I lived for a few months in Philippines (Manila) and also in Indonesia where I currently live, based in Jakarta.`,
       'resume': 'Resume'
+    },
+    'portfolio_detail': {
+      'presentation': `Durant 10 mois, avec mon équipe, nous avons travaillé sur un projet de création 
+                 d'un nouveau logiciel de gestion d'évènements. Le but était de refaire l'ensemble du design,
+                 de revoir le flow de l'application et ainsi améliorer l'ux. L'autre point était de remplacer 
+                 Jquery par le framework Vue.js, ainsi que Bootstrap par Bulma (framework css). Vue.js dans le 
+                 but de faire en sorte que l'application soit plus simplement maintenable et Bulma pour la lisibilité.
+                 Le back-end quand à lui était géré par Laravel. 
+                 
+                 Mon travail a donc été l'amélioration de l'expérience utilisateur, l'élaboration d'un nouveau design, puis
+                 le développement de l'application avec mon équipe.`,
+      'projet': `Mon travail fût principalement lié à la création graphique de documents, ainsi que d'un site web pour
+                pour la société (wordpress). J'ai donc élaboré des cartes de visites, des documents tel que des fiches produits, 
+                avec un design spécifique ainsi que la traduction de ces derniers.`,
+      'solution': `Mon travail en tant que freelance en 2010 fût d'abord principalement lié au développement ainsi qu'avec une part de design.
+                    Mes projets étaient réalisés from scratch. Depuis, après quelques années d'expériences, j'essaye de me consacrer
+                    au design, même si le développement front m'intéresse aussi. Je réalise aussi quelques logo, documents. 
+                    
+                    Je suis actuellement en freelance, depuis la fin de mon dernier projet en Suède, mais je suis à la recherche d'un post en tant
+                    que web designer, pouvant aussi travailler sur le front.`
     },
     'experiences': {
       'memlin': `During 10 months, with my team, we worked on a project to create a new event management software. The goal was to remake the entire design, 
--- lang/fr-FR.js
+++ lang/fr-FR.js
@@ -24,6 +24,10 @@ export default {
     'main_title': {
       'about_1': `J'aime le web design`,
       'about_2': `le front-end design`,
+      'portfolio_detail': `Quelques uns de mes projets`,
+      'portfolio_detail_sub_1': `Présentation`,
+      'portfolio_detail_sub_2': `Projet & besoin`,
+      'portfolio_detail_sub_3': `Solutions`,
       'portfolio': `Quelques réalisations`,
       'travels': `Je prends l'inspiration de mes voyages`
     },
@@ -31,11 +35,26 @@ export default {
       'about_1': `Je suis passionné par tout ce qui touche au web en ayant une préférence pour le design. Le développement, notamment le front est aussi important pour moi,
                   pour pouvoir réaliser et mettre en place ce que j'ai imaginé. J'ai donc des compétences en web design mais aussi en développement front, seo etc. Je suis aussi
                   intéressé par la vidéo, la photo ainsi que l'illustration.`,
-      'about_2': `Actuellement en recherche d'emploi au Quebec.`,
+      'about_2': `Actuellement en recherche d'emploi en France, au Quebec ou au Japon.`,
       'about_3': `En effet, actuellement en freelance depuis mon dernier poste en Suède, je cherche un poste de web designer au Quebec si possible ou en Asie tel que le Japon.`,
       'travels': `Voyager est l'une de mes passions même si je n'ai pas visité beaucoup de pays. J'ai vécu au Japon durant 1 année. 
                   J'ai ensuite vécu durant quelques mois aux Philippines à Manille ainsi qu'en Indonésie où je vis actuellement, basé à Jakarta`,
       'resume': 'CV'
+    },
+    'portfolio_detail': {
+      'presentation': `Memlin est une plate-forme de gestion d'événements en ligne.  
+                 
+                 Mon travail a été d'améliorer l'expérience utilisateur, d'élaborer un nouveau design, puis
+                 développer l'application avec mon équipe.`,
+      'projet': `Mon travail fût principalement lié à la création graphique de documents, ainsi que d'un site web pour
+                pour la société (wordpress). J'ai donc élaboré des cartes de visites, des documents tel que des fiches produits, 
+                avec un design spécifique ainsi que la traduction de ces derniers.`,
+      'solution': `Mon travail en tant que freelance en 2010 fût d'abord principalement lié au développement ainsi qu'avec une part de design.
+                    Mes projets étaient réalisés from scratch. Depuis, après quelques années d'expériences, j'essaye de me consacrer
+                    au design, même si le développement front m'intéresse aussi. Je réalise aussi quelques logo, documents. 
+                    
+                    Je suis actuellement en freelance, depuis la fin de mon dernier projet en Suède, mais je suis à la recherche d'un post en tant
+                    que web designer, pouvant aussi travailler sur le front.`
     },
     'experiences': {
       'memlin': `Durant 10 mois, avec mon équipe, nous avons travaillé sur un projet de création 
