8584508c3ffb3d2d39c4d2f9be5f92f1ded4b149
Damien <damien.jaillot@gmail.com>
2019-12-30 13:52:04 +0700

    home remake

--- pages/sections/Home.vue
+++ pages/sections/Home.vue
@@ -12,10 +12,11 @@
                             {{ $t('home.subtitle_1') }} {{ $t('home.subtitle_2') }}
                         </h2>
                         <div class="is-cta columns has-margin-x-0 has-margin-y-6">
-                            <a class="column is-6 is-cta-contact has-text-centered">
+                            <a href="mailto:damien.jaillot@gmail.com" class="column is-6 is-cta-contact has-text-centered">
                                 Contact me
                             </a>
-                            <a class="column is-6 is-cta-resume has-text-centered">
+                            <a href="/cv_damien.pdf" target="_blank" class="column is-6 is-cta-resume has-text-centered">
+                                <fa-icon class="has-margin-right-8px" :icon="['fa', 'file-pdf']" />
                                 My Resume
                             </a>
                         </div>
