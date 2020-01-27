fa196420429c818e81486023c18bd0efbffc4ae5
Damien <damien.jaillot@gmail.com>
2020-01-27 12:41:41 +0700

    lang file update

--- lang/fr-FR.js
+++ lang/fr-FR.js
@@ -1,77 +1,93 @@
 export default {
     'menu': {
-      'accueil': 'Accueil',
-      'a propos': 'A propos',
-      'portfolio': 'Portfolio',
-      'contact': 'Contact'
+      'accueil':              'Accueil',
+      'a propos':             'A propos',
+      'portfolio':            'Portfolio',
+      'contact':              'Contact'
     },
     'home': {
-      'hello': 'Bonjour',
-      'name': `DAMIEN JAILLOT`,
-      'subtitle_1': `UI/UX Designer &`,
-      'subtitle_2': `Front-End Développeur`,
-      'subtitle_3': `et aussi un peu illustrateur 😉`,
-      'contact': `Contactez moi`,
-      'resume': `Mon CV`,
+      'hello':                'Bonjour',
+      'name':                 `DAMIEN JAILLOT`,
+      'subtitle_1':           `UI/UX Designer &`,
+      'subtitle_2':           `Front-End Développeur`,
+      'subtitle_3':           `et aussi un peu illustrateur 😉`,
+      'contact':              `Contactez moi`,
+      'resume':               `Mon CV`,
     },
     'title': {
-      'about': 'A Propos',
-      'portfolio': 'Portfolio',
+      'about':                'A Propos',
+      'portfolio':            'Portfolio',
     },
     'main_title': {
-      'about_1': `UI/UX Designer`,
-      'about_2': `Front-end développeur`,
-      'portfolio_detail': `Mes projets`,
+      'about_1':              `UI/UX Designer`,
+      'about_2':              `Front-end développeur`,
+      'portfolio_detail':     `Mes projets`,
     },
     'text': {
-      'about_1': `Je suis passionné de design ainsi que tout ce qui touche au web en général, UX, développement, SEO, vidéo, illustration et autre. 
-                  Je ne passe pas une journée sans m'informer des nouveautés, des tendances etc. J'ai quelques années d'expérience maintenant que ça soit dans le design web mais aussi le 
-                  développement, notamment le développement front-end même si j'ai déjà travailler sur le back-end. Je cherche toujours à donner le meilleur de moi-même et à réaliser au mieux 
-                  chaque projet.`,
-      'about_2': `Actuellement en recherche de poste.`,
-      'about_3': `En effet, actuellement en freelance, je suis à la recherche d'une opportunité à temps plein en tant que #webdesigner, #uiuxdesigner ou #productdesigner.`,
-      'about_4': `N'hésitez pas à me contacter si vous êtes intéressé.`,
+      'about_1':              `Je suis passionné de design ainsi que tout ce qui touche au web en général, UX, développement, SEO, vidéo, illustration et autre. 
+                               Je ne passe pas une journée sans m'informer des nouveautés, des tendances etc. J'ai quelques années d'expérience maintenant que ça soit dans le design web mais aussi le 
+                               développement, notamment le développement front-end même si j'ai déjà travailler sur le back-end. Je cherche toujours à donner le meilleur de moi-même et à réaliser au mieux 
+                               chaque projet.`,
+      'about_2':              `Actuellement en recherche de poste.`,
+      'about_3':              `En effet, actuellement en freelance, je suis à la recherche d'une opportunité à temps plein en tant que #webdesigner, #uiuxdesigner ou #productdesigner.`,
+      'about_4':              `N'hésitez pas à me contacter si vous êtes intéressé.`,
     },
     'project': {
-      'subtitleMemlin': 'Système de gestion d\'événements convivial pour tous types d\'événements.',
+      'company':              'Société',
+      'service':              'Service',
+      'about':                'A popos de',
+      'problem':              'Projet & Problème',
+      'process':              'Processus & recherche',
+      'solution':             'Solution',
+      'Memlin': {
+        'subtitle':           'Système de gestion d\'événements pour tous types d\'événements.',
+        'company':            'Memlin AB, Suède',
+        'service':            'gestion d\'évènement, SAAS',
+        'about':              `Memlin was founded since there were no good systems for handling several different types of events. There were simpler registration systems, 
+                               education systems or really complex conferencing tools. 
+                               We found that many organizers compromised or used tools that were simply not effective because they were created for something else.`,
+        'problem':             '',
+        'process':             '',
+        'solution':            '',
+      },
     },
     'footer': {
-      'contact': 'Contactez moi',
-      'me': 'ici',
-      'text_1': 'Portfolio ',
-      'text_2': 'par ',
-      'text_3': '',
-      'text_4': 'Le code source est disponible sur ',
-      'text_5': '',
-      'mentions': 'Mentions Légales',
-      'rgpd': 'Confidentialité'
+      'contact':              'Contactez moi',
+      'me':                   'ici',
+      'text_1':               'Portfolio ',
+      'text_2':               'par ',
+      'text_3':               '',
+      'text_4':               'Le code source est disponible sur ',
+      'text_5':               '',
+      'mentions':             'Mentions Légales',
+      'rgpd':                 'Confidentialité'
     },
     'error': {
-      'first': 'Oups ! rien ici',
-      'second': 'Oups une erreur',
-      'third': 'Retour à la maison',
+      'first':                'Oups ! rien ici',
+      'second':               'Oups une erreur',
+      'third':                'Retour à la maison',
     },
     'cookie': {
-      'message': 'Ce site utilise votre location géographique pour définir votre langue la plus adaptée.',
-      'button': 'J\'ai compris',
+      'message':              'Ce site utilise votre location géographique pour définir votre langue la plus adaptée.',
+      'button':               'J\'ai compris',
       // 'click': 'En savoir plus',
     },
     'rgpd': {
-      'confidentialite': 'Confidentialité et gestion des Cookies',
-      'intro': 'Cette page vous permettra d\'en apprendre plus sur l\'utilisation et le fonctionnement des cookies présent sur mon portfolio.',
+      'confidentialite':      'Confidentialité et gestion des Cookies',
+      'intro':                'Cette page vous permettra d\'en apprendre plus sur l\'utilisation et le fonctionnement des cookies présent sur mon portfolio.',
       'cookies': {
-        'title': 'Un cookie, c\'est quoi ?',
-        'definition': `Un cookie est un petit fichier stocké votre navigateur Internet, échangé entre le site Internet que vous consultez et votre ordinateur et permettant 
-                        d\'enregistrer diverses informations. Ses principaux usages sont de faciliter la navigation, d\'activer certaines fonctionnalités ou encore d\'envoyer des données 
-                        d\'ordre statistique sur l\'utilisation et la fréquentation du site Internet.`,
-        'indispensables': 'Cookies fonctionnels',
-        'indi_text': `Les seuls cookies fonctionnels que nous utilisons sont:`,
-        'SERVERID': 'SERVERID...',
-        'servText': 'Un cookie technique utilisé par notre hébergeur (OVH) pour faire de la répartition de charge serveur.',
-        'i18n': 'i18n_redirected',
-        'i18nText': 'Un cookie technique utilisé sur notre site pour afficher notre site en Anglais ou Français selon votre pays.',
-        'confidentialite_2': 'Données personnelles',
-        'intro_2': `Aucune donnée personnelle n'est collectée.`,
+        'title':              'Un cookie, c\'est quoi ?',
+        'definition':         `Un cookie est un petit fichier stocké votre navigateur Internet, échangé entre le site Internet que vous consultez et votre ordinateur et permettant 
+                               d\'enregistrer diverses informations. Ses principaux usages sont de faciliter la navigation, d\'activer certaines fonctionnalités ou encore d\'envoyer des données 
+                               d\'ordre statistique sur l\'utilisation et la fréquentation du site Internet.`,
+        'indispensables':     'Cookies fonctionnels',
+        'indi_text':          `Les seuls cookies fonctionnels que nous utilisons sont:`,
+        'SERVERID':           'SERVERID...',
+        'servText':           'Un cookie technique utilisé par notre hébergeur (OVH) pour faire de la répartition de charge serveur.',
+        'i18n':               'i18n_redirected',
+        'i18nText':           'Un cookie technique utilisé sur notre site pour afficher notre site en Anglais ou Français selon votre pays.',
+        'confidentialite_2':  'Données personnelles',
+        'intro_2':            `Aucune donnée personnelle n'est collectée.`,
       },
     }
 }
